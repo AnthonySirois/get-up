93da2b2bf106633133f7df3d4d2d236379727fe1
Anthony Sirois <anthonysirois999@gmail.com>
2024-07-02 15:06:55 -0400

    change for schedule trait implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod notification;
 
-use std::{thread, time::Duration};
+use std::{ops::Range, thread, time::Duration};
 use clap::{Args, Parser, Subcommand};
 use rand::{distributions::{Distribution, Uniform}, thread_rng};
 
@@ -37,71 +37,104 @@ struct RandomArgs {
 fn main() {
     let cli = Cli::parse();
     
-    match cli.command {
-        Commands::Fixed(args) => { 
-            fixed_schedule(args, cli.verbose)
+    let schedule = parse_command(cli.command);
+
+    start_timer(schedule, cli.verbose);
+}
+
+fn parse_command(command : Commands) -> Box<dyn Schedule> {
+    match command {
+        Commands::Fixed(args) => {
+            let schedule = FixedSchedule{
+                seconds_sitting : Duration::from_secs(args.seconds_sitting.into()),
+                seconds_standing : Duration::from_secs(args.seconds_standing.into()),
+            };
+
+            Box::new(schedule)
         },
         Commands::Random(args) => {
-            random_schedule(args, cli.verbose)
+            let sitting_duration_range = Duration::from_secs(args.min_seconds_sitting.into())..Duration::from_secs(args.max_seconds_sitting.into());
+            let standing_duration_range = Duration::from_secs(args.min_seconds_sitting.into())..Duration::from_secs(args.max_seconds_sitting.into());
+
+            let schedule = RandomSchedule{
+                sitting_seconds_range : sitting_duration_range,
+                standing_seconds_range : standing_duration_range
+            };
+
+            Box::new(schedule)
         },
+    }
+}
+
+fn start_timer(schedule : Box<dyn Schedule>, verbose : bool) { 
+    let mut sitting: bool = true;
+
+    loop {
+        let sleep_duration = if sitting { schedule.get_sitting_duration() } else { schedule.get_standing_duration() };
+
+        if verbose {
+            let wake_up_time = format_wake_up_time(sleep_duration);
+            println!("Wake at {wake_up_time}");
+        }
+
+        thread::sleep(sleep_duration);
+
+        if sitting {
+            notification::send_stand_notification();
+        } else {
+            notification::send_sit_notification();
+        }
+
+        sitting = !sitting;
     }
 }
 
 const LONG_TIME_FORMAT : &str = "%H:%M:%S";
 
-fn fixed_schedule(args : FixedArgs, verbose : bool) {
-    let mut sitting = true;
+fn format_wake_up_time(sleep_duration : Duration) -> String {
+    let sleep_time = sleep_duration.as_secs();
 
-    let seconds_sitting = args.seconds_sitting;
-    let seconds_standing = args.seconds_standing;
+    let wait_time_delta = chrono::TimeDelta::try_seconds(sleep_time.try_into().unwrap_or_default()).unwrap_or_default();
+    let sleep_end_time = chrono::Local::now().checked_add_signed(wait_time_delta).unwrap_or_default();
+    
+    sleep_end_time.format(LONG_TIME_FORMAT).to_string()
+}
 
-    loop {
-        let wait_time = if sitting { seconds_sitting } else { seconds_standing };
-        let sleep_duration = Duration::from_secs(wait_time.into());
+trait Schedule {
+    fn get_sitting_duration(&self) -> Duration;
+    fn get_standing_duration(&self) -> Duration;
+}
 
-        if verbose {
-            let wait_time_delta = chrono::TimeDelta::try_seconds(wait_time.into()).unwrap_or_default();
-            let sleep_end_time = chrono::Local::now().checked_add_signed(wait_time_delta).unwrap_or_default();
-            println!("Time to awaken the empire : {}", sleep_end_time.format(LONG_TIME_FORMAT).to_string());
-        }
+struct FixedSchedule {
+    seconds_sitting : Duration,
+    seconds_standing : Duration,
+}
 
-        thread::sleep(sleep_duration);
+impl Schedule for FixedSchedule {
+    fn get_sitting_duration(&self) -> Duration {
+        self.seconds_sitting
+    }
 
-        if sitting {
-            notification::send_stand_notification();
-        } else {
-            notification::send_sit_notification();
-        }
-
-        sitting = !sitting;
+    fn get_standing_duration(&self) -> Duration {
+        self.seconds_standing
     }
 }
 
-fn random_schedule(args : RandomArgs, verbose : bool) {
-    let mut sitting = true;
-    let mut rng = thread_rng();
-    let between_sitting = Uniform::from(args.min_seconds_sitting..args.max_seconds_sitting);
-    let between_standing = Uniform::from(args.min_seconds_standing..args.max_seconds_standing);
+struct RandomSchedule {
+    sitting_seconds_range : Range<Duration>,
+    standing_seconds_range : Range<Duration>,
+}
+ 
+impl Schedule for RandomSchedule {
+    fn get_sitting_duration(&self) -> Duration {
+        let mut rng = thread_rng();
+        let between = Uniform::from(self.sitting_seconds_range.clone());
+        between.sample(&mut rng)
+    }
 
-    loop {
-        let wait_time = if sitting { between_sitting.sample(&mut rng) } else { between_standing.sample(&mut rng) };
-        
-        let sleep_duration = Duration::from_secs(wait_time.into());
-
-        if verbose {
-            let wait_time_delta = chrono::TimeDelta::try_seconds(wait_time.into()).unwrap_or_default();
-            let sleep_end_time = chrono::Local::now().checked_add_signed(wait_time_delta).unwrap_or_default();
-            println!("Time to awaken the empire : {}", sleep_end_time.format(LONG_TIME_FORMAT).to_string());
-        }
-
-        thread::sleep(sleep_duration);
-
-        if sitting {
-            notification::send_stand_notification();
-        } else {
-            notification::send_sit_notification();
-        }
-
-        sitting = !sitting;
+    fn get_standing_duration(&self) -> Duration {
+        let mut rng = thread_rng();
+        let between = Uniform::from(self.standing_seconds_range.clone());
+        between.sample(&mut rng)
     }
 }
