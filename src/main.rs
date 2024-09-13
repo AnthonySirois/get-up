d5adefaa59de3af76ca7a4e3c0963fca3ac5c38f
Anthony Sirois <anthonysirois999@gmail.com>
2024-09-13 10:06:45 -0400

    change to Elm architecture

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,177 @@
 mod notification;
 
-use std::{ops::Range, thread, time::Duration, io};
+use std::{default, io, thread, time::Duration};
 use crossterm::{event::KeyEvent, terminal};
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind}, 
-    style::{Color, Modifier, Style, Stylize}, 
+    style::{Color, Style, Stylize}, 
     layout::{Alignment, Rect},
-    widgets::{block::{Title, Position}, Paragraph, Widget, Block, LineGauge, Gauge},
-    text::{Line, Text},
+    widgets::{block::{Title, Position}, Widget, Block, LineGauge},
+    text::Line,
     DefaultTerminal, 
     Frame, 
     symbols::border,
-    Terminal,
     prelude::{Buffer, symbols, Layout, Direction, Constraint},
 };
+
+#[derive(Debug, Default)]
+struct Model {
+    running_state : RunningState
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+enum RunningState {
+    #[default]
+    Running,
+    Done
+}
+
+enum Message {
+    Increase,
+    Decrease,
+    Reset,
+    Next,
+    Quit,
+    Pause,
+    Resume,
+    Navigate
+}
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     let _ = terminal.clear()?;
-    let app_result = App::default().run(&mut terminal);
-    ratatui::restore();
 
-    app_result
+    let mut model = Model::default();
+
+    while model.running_state != RunningState::Done {
+        terminal.draw(|frame| view(&model, frame))?;
+
+        let mut current_message = handle_events(&model)?;
+
+        while current_message.is_some() {
+            current_message = update(&mut model, current_message.unwrap());
+        }
+    }
+
+    ratatui::restore();
+    
+    Ok(())
+}
+
+fn view(model : &Model, frame : &mut Frame) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage(50),
+            Constraint::Percentage(50),
+        ])
+        .spacing(1)
+        .split(frame.area());
+
+    let option_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Percentage(50),
+                Constraint::Percentage(50)
+            ])
+            .spacing(1)
+            .split(chunks[1]);
+
+    let progress_title = Title::from(" GET UP : Standing until X ".bold());
+    let progress_instructions = Title::from(Line::from(vec![
+        " Quit ".into(),
+        "<Q>".blue().bold(),
+        " Pause/Resume ".into(),
+        "<Space>".blue().bold(),
+        " Restart ".into(),
+        "<H>".blue().into(),
+        " Next ".into(),
+        "<L> ".blue().into()
+    ]));
+    let progress_block = Block::bordered()
+                .title(progress_title.alignment(Alignment::Center))
+                .title(
+                    progress_instructions
+                        .alignment(Alignment::Center)
+                        .position(Position::Bottom
+                        ))
+                .border_set(border::THICK);
+
+    frame.render_widget(
+        LineGauge::default()
+            .block(progress_block)
+            .filled_style(Style::default().fg(Color::Green))
+            .line_set(symbols::line::DOUBLE)
+            .label("[PAUSED] 15m13")
+            .ratio(0.4), 
+        chunks[0]
+    );
+    
+
+    let option_instructions = Title::from(Line::from(vec![
+        " Decrease ".into(),
+        "<H>".blue().into(),
+        " Increase ".into(),
+        "<L> ".blue().into(),
+    ]))
+        .alignment(Alignment::Center)
+        .position(Position::Bottom);
+
+    let sitting_option_title = Title::from(" Sitting duration ".bold());
+    let sitting_option_block = Block::bordered()
+        .title(sitting_option_title.alignment(Alignment::Center))
+        .title(option_instructions.clone())
+        .border_set(border::THICK);
+
+    frame.render_widget(
+        LineGauge::default()
+            .block(sitting_option_block)
+            .filled_style(Style::default().fg(Color::Blue))
+            .line_set(symbols::line::NORMAL)
+            .label("1h00m")
+            .ratio(0.25), 
+        option_chunks[0]
+    );
+
+    let standing_option_title = Title::from(" Standing duration ".bold());
+    let standing_option_block = Block::bordered()
+        .title(standing_option_title.alignment(Alignment::Center))
+        .title(option_instructions.clone())
+        .border_set(border::THICK);
+
+    frame.render_widget(
+        LineGauge::default()
+            .block(standing_option_block)
+            .filled_style(Style::default().fg(Color::Blue))
+            .line_set(symbols::line::NORMAL)
+            .label("0h30m")
+            .ratio(0.25), 
+        option_chunks[1]
+    );
+}
+
+fn handle_events(model : &Model) -> io::Result<Option<Message>> {
+    if event::poll(Duration::from_millis(250))? {
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Char('Q') => return Ok(Some(Message::Quit)),
+                    KeyCode::Char(' ') => return Ok(Some(Message::Resume)), // TODO Depend on state
+                    _ => {}
+                }
+            }
+        }
+    }
+    Ok(None)
+}
+
+fn update(model : &mut Model, message : Message) -> Option<Message> {
+    match message {
+        Message::Quit => model.running_state = RunningState::Done,
+        _ => {}
+    }
+
+    None
 }
 
 #[derive(Debug, Default)]
@@ -38,124 +188,6 @@ struct App {
     state : State,
     standing_time_minutes : i32,
     sitting_time_minutes : i32
-}
-
-impl App {
-    fn run(&mut self, terminal : &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        if event::poll(Duration::from_millis(1000))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    match key.code {
-                        KeyCode::Char('q') | KeyCode::Char('Q') => self.exit = true,
-                        KeyCode::Char(' ') => self.is_paused = !self.is_paused,
-                        _ => {}
-                    }
-                }
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Percentage(50),
-                Constraint::Percentage(50),
-            ])
-            .spacing(1)
-            .split(area);
-
-        let option_chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([
-                    Constraint::Percentage(50),
-                    Constraint::Percentage(50)
-                ])
-                .spacing(1)
-                .split(chunks[1]);
-
-        let progress_title = Title::from(" GET UP : Standing until X ".bold());
-        let progress_instructions = Title::from(Line::from(vec![
-            " Quit ".into(),
-            "<Q>".blue().bold(),
-            " Pause/Resume ".into(),
-            "<Space>".blue().bold(),
-            " Restart ".into(),
-            "<H>".blue().into(),
-            " Next ".into(),
-            "<L> ".blue().into()
-        ]));
-        let progress_block = Block::bordered()
-                    .title(progress_title.alignment(Alignment::Center))
-                    .title(
-                        progress_instructions
-                            .alignment(Alignment::Center)
-                            .position(Position::Bottom
-                            ))
-                    .border_set(border::THICK);
-
-        LineGauge::default()
-            .block(progress_block)
-            .filled_style(Style::default().fg(Color::Green))
-            .line_set(symbols::line::DOUBLE)
-            .label("[PAUSED] 15m13")
-            .ratio(0.4)
-            .render(chunks[0], buf);
-
-        let option_instructions = Title::from(Line::from(vec![
-            " Decrease ".into(),
-            "<H>".blue().into(),
-            " Increase ".into(),
-            "<L> ".blue().into(),
-        ]))
-            .alignment(Alignment::Center)
-            .position(Position::Bottom);
-
-        let sitting_option_title = Title::from(" Sitting duration ".bold());
-        let sitting_option_block = Block::bordered()
-            .title(sitting_option_title.alignment(Alignment::Center))
-            .title(option_instructions.clone())
-            .border_set(border::THICK);
-
-        LineGauge::default()
-            .block(sitting_option_block)
-            .filled_style(Style::default().fg(Color::Blue))
-            .line_set(symbols::line::NORMAL)
-            .label("1h00m")
-            .ratio(0.25)
-            .render(option_chunks[0], buf);
-
-        let standing_option_title = Title::from(" Standing duration ".bold());
-        let standing_option_block = Block::bordered()
-            .title(standing_option_title.alignment(Alignment::Center))
-            .title(option_instructions.clone())
-            .border_set(border::THICK);
-
-        LineGauge::default()
-            .block(standing_option_block)
-            .filled_style(Style::default().fg(Color::Blue))
-            .line_set(symbols::line::NORMAL)
-            .label("0h30m")
-            .ratio(0.25)
-            .render(option_chunks[1], buf);
-    }
 }
 
 fn cli_main() {    
