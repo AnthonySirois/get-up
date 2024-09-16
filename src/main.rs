123af2f7c064dd8aecb778b57fd710bf9bae3b32
Anthony Sirois <anthonysirois999@gmail.com>
2024-09-16 10:20:55 -0400

    add timer and implement into application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod notification;
+mod pausable_timer;
 
 use crossterm::{event::KeyEvent, terminal};
+use pausable_timer::Timer;
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     layout::Alignment,
@@ -19,7 +21,7 @@ use std::{io, thread, time::Duration};
 const INCREASE_STEP_DURATION: Duration = Duration::from_secs(300);
 const POLL_DURATION: Duration = Duration::from_millis(250);
 const MAX_DURATION: Duration = Duration::from_secs(14400);
-const MIN_DURATION: Duration = Duration::from_secs(300);
+const MIN_DURATION: Duration = Duration::from_secs(10); // TODO Put back to 300
 
 const TITLE_STYLE: Style = Style::new().fg(Color::LightCyan);
 const SELECTED_STYLE: Style = Style::new().fg(Color::Rgb(202, 166, 247));
@@ -36,6 +38,7 @@ struct Model {
 
     running_state: RunningState,
     selected_widget_block: WidgetBlock,
+    timer: Timer,
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -76,6 +79,7 @@ enum Message {
     Pause,
     Resume,
     Navigate,
+    TimeCycleEnd,
 }
 
 fn main() -> io::Result<()> {
@@ -152,13 +156,34 @@ fn view(model: &Model, frame: &mut Frame) {
         .title_style(TITLE_STYLE)
         .border_set(border::THICK);
 
+    let ratio = (model.timer.elapsed().as_secs_f64() / model.sitting_duration.as_secs_f64())
+        .clamp(0.0, 1.0);
+
+    let timer_duration = match model.state {
+        State::Sitting => model.sitting_duration,
+        State::Standing => model.standing_duration,
+    };
+    let time_left = timer_duration.saturating_sub(model.timer.elapsed());
+
     frame.render_widget(
         LineGauge::default()
             .block(progress_block)
-            .filled_style(if model.timer_state == TimerState::InProgress { IN_PROGRESS_GAUGE_STYLE } else { PAUSED_GAUGE_STYLE })
+            .filled_style(if model.timer_state == TimerState::InProgress {
+                IN_PROGRESS_GAUGE_STYLE
+            } else {
+                PAUSED_GAUGE_STYLE
+            })
             .line_set(symbols::line::DOUBLE)
-            .label(format!("{} 15m13", if model.timer_state == TimerState::Paused { "[PAUSED]"} else { "        " }))
-            .ratio(0.4),
+            .label(format!(
+                "{} {}",
+                if model.timer_state == TimerState::Paused {
+                    "[PAUSED]"
+                } else {
+                    "        "
+                },
+                format_duration_hours_minutes_seconds(time_left)
+            ))
+            .ratio(ratio),
         chunks[0],
     );
 
@@ -191,8 +216,12 @@ fn view(model: &Model, frame: &mut Frame) {
             .block(sitting_option_block)
             .filled_style(Style::default().fg(Color::Blue))
             .line_set(symbols::line::NORMAL)
-            .label(format_duration_hours_minutes(&model.sitting_duration))
-            .ratio(ratio_duration(model.sitting_duration, MIN_DURATION, MAX_DURATION)),
+            .label(format_duration_hours_minutes(model.sitting_duration))
+            .ratio(ratio_duration(
+                model.sitting_duration,
+                MIN_DURATION,
+                MAX_DURATION,
+            )),
         option_chunks[0],
     );
 
@@ -211,19 +240,31 @@ fn view(model: &Model, frame: &mut Frame) {
         .title_style(TITLE_STYLE)
         .border_set(border::THICK);
 
-
     frame.render_widget(
         LineGauge::default()
             .block(standing_option_block)
             .filled_style(Style::default().fg(Color::Blue))
             .line_set(symbols::line::NORMAL)
-            .label(format_duration_hours_minutes(&model.standing_duration))
-            .ratio(ratio_duration(model.standing_duration, MIN_DURATION, MAX_DURATION)),
+            .label(format_duration_hours_minutes(model.standing_duration))
+            .ratio(ratio_duration(
+                model.standing_duration,
+                MIN_DURATION,
+                MAX_DURATION,
+            )),
         option_chunks[1],
     );
 }
 
 fn handle_events(model: &Model) -> io::Result<Option<Message>> {
+    let timer_duration = if model.state == State::Sitting {
+        model.sitting_duration
+    } else {
+        model.standing_duration
+    };
+    if model.timer.elapsed() > timer_duration {
+        return Ok(Some(Message::TimeCycleEnd));
+    }
+
     if event::poll(POLL_DURATION)? {
         if let event::Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Press {
@@ -264,36 +305,42 @@ fn update(model: &mut Model, message: Message) -> Option<Message> {
         Message::Quit => model.running_state = RunningState::Done,
         Message::Increase => match model.selected_widget_block {
             WidgetBlock::SittingOption => {
-                let new_duration = model
+                model.sitting_duration = model
                     .sitting_duration
-                    .saturating_add(INCREASE_STEP_DURATION);
-                model.sitting_duration = clamp_duration(new_duration, MIN_DURATION, MAX_DURATION)
+                    .saturating_add(INCREASE_STEP_DURATION)
+                    .clamp(MIN_DURATION, MAX_DURATION);
             }
             WidgetBlock::StandingOption => {
-                let new_duration = model
+                model.standing_duration = model
                     .standing_duration
-                    .saturating_add(INCREASE_STEP_DURATION);
-                model.standing_duration = clamp_duration(new_duration, MIN_DURATION, MAX_DURATION)
+                    .saturating_add(INCREASE_STEP_DURATION)
+                    .clamp(MIN_DURATION, MAX_DURATION);
             }
             _ => {}
         },
         Message::Decrease => match model.selected_widget_block {
             WidgetBlock::SittingOption => {
-                let new_duration = model
+                model.sitting_duration = model
                     .sitting_duration
-                    .saturating_sub(INCREASE_STEP_DURATION);
-                model.sitting_duration = clamp_duration(new_duration, MIN_DURATION, MAX_DURATION)
+                    .saturating_sub(INCREASE_STEP_DURATION)
+                    .clamp(MIN_DURATION, MAX_DURATION);
             }
             WidgetBlock::StandingOption => {
-                let new_duration = model
+                model.standing_duration = model
                     .standing_duration
-                    .saturating_sub(INCREASE_STEP_DURATION);
-                model.standing_duration = clamp_duration(new_duration, MIN_DURATION, MAX_DURATION)
+                    .saturating_sub(INCREASE_STEP_DURATION)
+                    .clamp(MIN_DURATION, MAX_DURATION);
             }
             _ => {}
         },
-        Message::Pause => model.timer_state = TimerState::Paused,
-        Message::Resume => model.timer_state = TimerState::InProgress,
+        Message::Pause => {
+            model.timer_state = TimerState::Paused;
+            model.timer.pause();
+        }
+        Message::Resume => {
+            model.timer_state = TimerState::InProgress;
+            model.timer.resume();
+        }
         Message::Navigate => {
             model.selected_widget_block = match model.selected_widget_block {
                 WidgetBlock::Timer => WidgetBlock::SittingOption,
@@ -301,32 +348,50 @@ fn update(model: &mut Model, message: Message) -> Option<Message> {
                 WidgetBlock::StandingOption => WidgetBlock::Timer,
             }
         }
-        Message::Next => {}
-        Message::Reset => {}
+        Message::Next => {
+            model.timer.reset();
+            model.state = match model.state {
+                State::Sitting => State::Standing,
+                State::Standing => State::Sitting,
+            };
+        }
+        Message::Reset => {
+            model.timer.reset();
+        }
+        Message::TimeCycleEnd => {
+            model.timer.reset();
+            model.state = match model.state {
+                State::Sitting => State::Standing,
+                State::Standing => State::Sitting,
+            };
+
+            match model.state {
+                State::Sitting => notification::send_sit_notification(model.sitting_duration),
+                State::Standing => notification::send_stand_notification(model.standing_duration),
+            };
+        }
     }
 
     None
-}
-
-fn clamp_duration(duration: Duration, min: Duration, max: Duration) -> Duration {
-    if duration < min {
-        min
-    } else if duration > max {
-        max
-    } else {
-        duration
-    }
 }
 
 fn ratio_duration(duration: Duration, min: Duration, max: Duration) -> f64 {
     (duration.as_secs_f64() - min.as_secs_f64()) / (max.as_secs_f64() - min.as_secs_f64())
 }
 
-fn format_duration_hours_minutes(duration: &Duration) -> String {
+fn format_duration_hours_minutes(duration: Duration) -> String {
     let hours = duration.as_secs() / 3600;
     let minutes = (duration.as_secs() / 60) % 60;
 
     format!("{}h{}m", hours, minutes)
+}
+
+fn format_duration_hours_minutes_seconds(duration: Duration) -> String {
+    let hours = duration.as_secs() / 3600;
+    let minutes = (duration.as_secs() / 60) % 60;
+    let seconds = duration.as_secs() % 60;
+
+    format!("{}h{}m{}s", hours, minutes, seconds)
 }
 
 #[cfg(test)]
@@ -434,104 +499,5 @@ mod tests {
         update(&mut model, Message::Navigate);
 
         assert_eq!(model.selected_widget_block, WidgetBlock::Timer);
-    }
-
-    #[test]
-    fn test_clamp_duration_lower_min() {
-        let duration = Duration::from_secs(300);
-        let min_duration = Duration::from_secs(600);
-
-        let result = clamp_duration(duration, min_duration, Duration::MAX);
-
-        assert_eq!(min_duration, result);
-    }
-
-    #[test]
-    fn test_clamp_duration_higher_max() {
-        let duration = Duration::from_secs(600);
-        let max_duration = Duration::from_secs(300);
-
-        let result = clamp_duration(duration, Duration::from_secs(0), max_duration);
-
-        assert_eq!(max_duration, result);
-    }
-
-    #[test]
-    fn test_clamp_duration_between_limits() {
-        let duration = Duration::from_secs(600);
-        let min_duration = Duration::from_secs(300);
-        let max_duration = Duration::from_secs(900);
-
-
-        let result = clamp_duration(duration, min_duration, max_duration);
-
-        assert_eq!(duration, result);
-    }
-}
-
-// ------------------------------------------------------------------------------
-
-fn start_timer(schedule: Box<dyn Schedule>, verbose: bool, sitting: bool) {
-    let mut sitting: bool = sitting;
-
-    loop {
-        let sleep_duration = if sitting {
-            schedule.get_sitting_duration()
-        } else {
-            schedule.get_standing_duration()
-        };
-
-        if verbose {
-            let wake_up_time = format_time_after_duration(sleep_duration);
-            if sitting {
-                println!("Sitting until {wake_up_time}");
-            } else {
-                println!("Standing until {wake_up_time}");
-            }
-        }
-
-        if sitting {
-            notification::send_sit_notification(sleep_duration);
-        } else {
-            notification::send_stand_notification(sleep_duration);
-        }
-
-        thread::sleep(sleep_duration);
-
-        sitting = !sitting;
-    }
-}
-
-const LONG_TIME_FORMAT: &str = "%H:%M:%S";
-
-fn format_time_after_duration(duration: Duration) -> String {
-    let sleep_time = duration.as_secs();
-
-    let wait_time_delta = chrono::TimeDelta::try_seconds(sleep_time.try_into().unwrap_or_default())
-        .unwrap_or_default();
-    let sleep_end_time = chrono::Local::now()
-        .checked_add_signed(wait_time_delta)
-        .unwrap_or_default();
-
-    sleep_end_time.format(LONG_TIME_FORMAT).to_string()
-}
-
-trait Schedule {
-    fn get_sitting_duration(&self) -> Duration;
-    fn get_standing_duration(&self) -> Duration;
-}
-
-struct FixedSchedule {
-    sitting_duration: Duration,
-    standing_duration: Duration,
-}
-
-impl Schedule for FixedSchedule {
-    fn get_sitting_duration(&self) -> Duration {
-        self.sitting_duration
-    }
-
-    fn get_standing_duration(&self) -> Duration {
-        self.standing_duration
     }
 }
